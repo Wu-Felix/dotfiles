9b92abbf5cedf5d62297207fe6a547a94ad0e180
felix <Wu-Felix@outlook.com>
2024-10-31 16:55:57 +0800

    feat(powershell):PSCOmpletions

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,8 @@ Set-alias 'sudo' 'gsudo'
 # 引入 posh-git
 Import-Module posh-git
 
+Import-Module PSCompletions
+
 # Import the catppuccin
 Import-Module Catppuccin
 
@@ -19,7 +21,7 @@ Set-PSReadLineOption -PredictionSource History
 # # 每次回溯输入历史，光标定位于输入内容末尾
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 # # 设置 Tab 为菜单补全和 Intellisense
-Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+#Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
 Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
 Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
 Set-PSReadLineKeyHandler -Chord '"',"'" `
