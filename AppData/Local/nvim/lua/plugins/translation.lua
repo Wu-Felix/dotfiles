390982dada1d695bb79af400f02d92b940bd0c84
felix <Wu-Felix@outlook.com>
2024-11-13 16:41:35 +0800

    feat(nvim):trans

--- AppData/Local/nvim/lua/plugins/translation.lua
+++ AppData/Local/nvim/lua/plugins/translation.lua
@@ -58,4 +58,23 @@ return {
       vim.keymap.set("x", "<leader>ir", pantran.motion_translate, opts)
     end,
   },
+  {
+    "JuanZoran/Trans.nvim",
+    lazy = false,
+    build = function()
+      require("Trans").install()
+    end,
+    keys = {
+      -- 可以换成其他你想映射的键
+      { "<leader>fy", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = "󰊿 Translate" },
+      -- { "<leader>mk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
+      -- -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
+      -- { "<leader>mi", "<Cmd>TranslateInput<CR>", desc = "󰊿 Translate From Input" },
+    },
+    dependencies = { "kkharji/sqlite.lua" },
+    opts = {
+      dir = os.getenv("home") .. "\\OneDrive\\.config\\dict",
+      -- your configuration there
+    },
+  },
 }
