6ce30307092ac71f1b778d2bf5b0bb7e830fa39b
felix <Wu-Felix@outlook.com>
2025-02-19 09:27:29 +0800

    refactor(nvim):obsidian markdown

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -13,8 +13,14 @@ return {
         position = "inline",
       },
       checkbox = {
+        enabled = true,
+        right_pad = 0,
         custom = {
-          important = { raw = "[>]", rendered = "", highlight = "DiagnosticWarn" },
+          question = { raw = "[?]", rendered = "", highlight = "RenderMarkdownError" },
+          ongoing = { raw = "[>]", rendered = "", highlight = "RenderMarkdownSuccess" },
+          canceled = { raw = "[~]", rendered = "󰗨", highlight = "ObsidianTilde" },
+          important = { raw = "[!]", rendered = "", highlight = "RenderMarkdownWarn" },
+          favorite = { raw = "[^]", rendered = "", highlight = "RenderMarkdownMath" },
         },
       },
     },
