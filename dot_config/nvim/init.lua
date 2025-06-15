f9418e6bc126eddf61f67547868a6f1459bb394a
felix <Wu-Felix@outlook.com>
2025-06-15 18:58:27 +0800

    ✨ feat(nvim): 增加 snacks 调试与 profiler 支持
    为 init.lua 增加 snacks 调试函数（dd、bt、p）及 snacks profiler
    的启动逻辑。支持通过环境变量 PROF 启用 profiler，并兼容 VSCode
    环境。优化启动流程，提升调试与性能分析体验。

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,35 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.env.VSCODE then
+  vim.g.vscode = true
+end
+
+if vim.loader then
+  vim.loader.enable()
+end
+
+_G.dd = function(...)
+  require("snacks.debug").inspect(...)
+end
+_G.bt = function(...)
+  require("snacks.debug").backtrace()
+end
+_G.p = function(...)
+  require("snacks.debug").profile(...)
+end
+vim.print = _G.dd
+
+if vim.env.PROF then
+  vim.opt.rtp:append("~/.local/share/nvim/lazy/snacks.nvim/")
+  require("snacks.profiler").startup({
+    startup = {
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+    -- runtime = "~/projects/neovim/runtime",
+  })
+end
+
+pcall(require, "config.env")
+
 require("config.lazy")
 require("utils.config")
